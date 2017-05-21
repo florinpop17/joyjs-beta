550e8f80177492831fb7fa9ccdc970bfa9bb8949
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-21 11:01:40 +0300

    userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
     date_created: { type: Date, default: Date.now },
-    text: { type: String, required: true },
-    correct: { type: String, required: true },
-    creator: { type: String, default: "Joy ^_^" }
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
